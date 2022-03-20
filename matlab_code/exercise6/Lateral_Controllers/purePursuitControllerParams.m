c5d0bbf57f552cbe09c4ef0b4403ff04954e790d
basemprince <bshaker@kth.se>
2022-03-20 03:35:33 +0100

    merge

---
+++ matlab_code/exercise6/Lateral_Controllers/purePursuitControllerParams.m
@@ -0,0 +1,11 @@
+function purePursuitParams = purePursuitControllerParams(lookAhead)
+
+    % ----------------------------------------------------------------
+    %% Function purpose: define the pure pursuit lateral controller parameters
+    % ----------------------------------------------------------------
+    
+    % Look ahead distance (in meters) used by the controller
+    purePursuitParams.lookAhead = lookAhead; 
+
+end
+
